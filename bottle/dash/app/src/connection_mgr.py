9817b3cb051cab952bc02f34f0c7aff92a9ff8af
Dan Deckert <dd27@txstate.edu>
2015-07-10 12:28:42 -0500

    added connection_mgr class

---
+++ bottle/dash/app/src/connection_mgr.py
@@ -0,0 +1,11 @@
+from sys_info_processor.py import SysInfoProcessor
+from settings.mysql_connector import MySqlConnector
+
+
+class ConnectionMgr:
+
+	def __init__(self):
+
+		json = s.decodeJson()
+
+
